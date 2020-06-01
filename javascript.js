a19d18f0a6d004da2333a0bc35ea550ab1e3dc5f
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-01 19:30:49 +0800

    set up questions in js

---
+++ javascript.js
@@ -0,0 +1,55 @@
+// Set-up question using Array
+
+let question = [{
+    question1: "What was the first Disney film that was produced in color?",
+    choiceA: "Cinderella",
+    choiceB: "Snow White and the Seven Dwafs",
+    choiceC: "Sleeping Beauty",
+    choiceD: "Pocahontas",
+    correct: "B"
+},
+
+{
+    question2: "What is celebrated on December 26th?",
+    choiceA: "the day after Christmas",
+    choiceB: "Harvest Day",
+    choiceC: "Boxing Day",
+    choiceD: "National Dog Day",
+    correct: "C"
+},
+
+{
+    question3: "How many rings are there in the Olympic symbol?",
+    choiceA: "5",
+    choiceB: "7",
+    choiceC: "4",
+    choiceD: "9",
+    correct: "7"
+},
+
+{
+    question4: "How many time zones are there in the world?",
+    choiceA: "7",
+    choiceB: "24",
+    choiceC: "23",
+    choiceD: "9",
+    correct: "B"
+},
+
+{
+    question5: "Han Solo is a character from which movie series?",
+    choiceA: "Harry Porter",
+    choiceB: "Star Wars",
+    choiceC: "Lord of the Rings",
+    choiceD: "Indiana Jones",
+    correct: "B"
+},
+
+{
+    question6: "What is the longest river in the world?",
+    choiceA: "Amazon",
+    choiceB: "Congo",
+    choiceC: "Nile",
+    choiceD: "Hudson",
+    correct: "C"
+}]
