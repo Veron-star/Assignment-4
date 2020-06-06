85f36b3f41a38b182b62f8bfbd68e87bb75cc5d4
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-06 23:47:59 +0800

    push

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-Exiting Quiz Game by Veronica 
+Exiting Quiz Game
 
 Design multiple choice quiz game using the following method:
 - Array
 - Loop
-- Render
+- Score
 - Timer
-- Bootstray
-- Media Quiry
 
-This exiciting quiz game consists of six questions, timer and score board.
 
-URL:
 
-GitHub:
 
+URL: https://veron-star.github.io/quizgame/
+
+GitHub: https://github.com/Veron-star/quizgame
 
 
 
@@ -21,23 +19,5 @@ GitHub:
 
 
 
-## Acceptance Criteria
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
 
--- javascript.js
+++ javascript.js
@@ -1,16 +1,24 @@
 // Set-up question using Array
 
 const questionText = document.querySelector(".question-text");
-const optionBox = document.querySelector(".option-box");
 const currentQuestion = document.querySelector(".current-question");
+const optionBox = document.querySelector(".option-box");
 const nextQuestionBtn = document.querySelector(".next-question-btn");
 const nextQuestion = document.querySelector(".next-question");
 const correctAnswer = document.querySelector(".correct-answer");
 const resultBtn = document.querySelector(".result-btn");
+const scoreText = document.querySelector(".score-text");
 const remainingTime = document.querySelector(".remaining-time");
+const questionBox = document.querySelector(".question-box");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const quizOverBox = document.querySelector(".quiz-over-box");
+const tryAgainBtn = document.querySelector(".try-again-btn");
+const startBtn = document.querySelector(".start-btn");
 let questionIndex = 0;
 let score = 0;
 let number = 0;
+let myApp = [];
 
 myApp = [{
     question:"What was the first Disney film that was produced in color?",
@@ -49,19 +57,19 @@ myApp = [{
 }];
 
 function load(){
-    number++;
+    // number++;
     questionText.innerHTML=myApp[questionIndex].question;
     createOptions();
-    scoreBoard();
+    scoreText();
     currentQuestion.innerHTML=number + myApp.length;
 }
 
 function createOptions(){
-    optionBox.innerHTML="";
+    // optionBox.innerHTML="";
     for(let i=0; i<myApp[questionIndex].options.length; i++){
         const option=document.createElement("div");
               option.innerHTML=myApp[questionIndex].options[i];
-              option.classList.add("options");
+              option.classList.add("option");
               option.id=i;
               option.setAttribute("onclick","check(this)");
               optionBox.appendChild(option);
@@ -73,15 +81,23 @@ function check(ele){
     if(id==myApp[questionIndex].answer){
         ele.classList.add("correct");
         score++;
-        scoreBoard();
+        scoreText();
     }
     else{
-        ele.classList.add("incorrect");}
-
+        ele.classList.add("incorrect");
+        for(let i=0; i<optionBox.children.length; i++){
+            if(optionBox.children[i].id==myApp[questionIndex].answer){
+                optionBox.children[i].classList.add("show-correct");
+            };
+        }
+    }
+    
         disableOptions()
+        showNextQuestionBtn();
+        stopTimer();
 
     if(number == myApp.length){
-        quiz-over();
+        quizOver();
     }
 }
 
@@ -101,8 +117,8 @@ function startTimer(){
 }
 
 function disableOptions(){
-    for(let i=0; i<optionBox.children.length; i++){
-        optionBox.children[i].classList.add("you have answered");
+    for(let i=0; i<questionBox.children.length; i++){
+        questionBox.children[i].classList.add("you have answered");
     }
 }
 
@@ -120,9 +136,25 @@ function scoreBoard(){
 
 nextQuestionBtn.addEventListener("click",nextQuestion);
 
-function nextQuestion(){
-    load();
-    hideNextQuestionBtn();
+// function nextQuestion(){
+//     hideNextQuestionBtn();
+//     startTimer();
+// }
+
+function quizResult(){
+    document.querySelector(".total-questions").innerHTML=myApp.length;
+    document.querySelector(".score:").innerHTML=myApp.length;
+    document.querySelector(".correct:").innerHTML=score;
+    document.querySelector(".wrong:").innerHTML=score;
+    const percentage=(score/myApp.length)*100;
+    document.querySelector(".percentage:").innerHTML=percentage.toFixed(2) + "%";
+}
+
+function resetQuiz(){
+    
+    score = 0;
+    number = 0;
+    myApp = [];
 }
 
 function quizOver(){
@@ -130,7 +162,21 @@ function quizOver(){
     resultBtn.classList.add("show");
 }
 
+resultBtn.addEventListener("click",()=>{
+    quizBox.style.display="none";
+    resultBtn.classList.remove("show");
+    quizOverBox.classList.add("show");
+    quizResult();
+})
 
-window.onload=()=>{
-    load();
-}
+tryAgainBtn.addEventListener("click", ()=>{
+    quizBox.classList.add("show");
+    quizOverBox.classList.remove("show");
+    resetQuiz();
+    // nextQuestion();
+})
+
+startBtn.addEventListener("click",()=>{
+    quizBox.classList.add("show");
+    startTimer();
+})
